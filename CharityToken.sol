460ffa6788691d723aa3e2db44c4442182d37c75
Griff Green <saveoursonics@gmail.com>
2016-10-25 01:52:44 +0700

    Added comments, changed ChT to GIVE
    Prelim comments until I fully understand the set of contracts... then I will add the @param @dev @notice format :-)

--- CharityToken.sol
+++ CharityToken.sol
@@ -2,30 +2,48 @@ pragma solidity ^0.4.0;
 
 import "HumanStandardToken.sol";
 
+// HumanStandardToken is Consensys's ERC 20 compliant token contract with the 
+// approveAndCall function omitted
 
 contract CharityToken is HumanStandardToken {
 
+// The creator is the address that deployed the Charity Token, for Charity DAO
+// it will be the Campaign Contract  
     address public creator;
+
+// onlyCreator is a modifier that only allows the creator to call the function
 
     modifier onlyCreator { if (msg.sender != creator) throw; _; }
 
-    function CharityToken() HumanStandardToken(0,"Charity Token",18,"ChT") {
+// charityToken is the function that deploys a new token 
+
+    function CharityToken() HumanStandardToken(0,"Charity Token",18,"GIVE") {
         creator = msg.sender;
     }
 
-    function createTokens(address beneficiary, uint amount) onlyCreator returns (bool success) {
-        if (sealed()) throw;
-        balances[beneficiary] += amount;               // Give the creator all initial tokens
-        totalSupply += amount;                        // Update total supply
-        Transfer(0, beneficiary, amount);             // Simulate a creation as a Transfer from 0.
-        return true;
+// createTokens takes parameters beneficiary (the address receiving the tokens) 
+// and amount (the amount of tokens the address is receiving) that can only be 
+// called by the creator if the campaign has not been sealed. If it works it 
+// will return success == true
+
+    function createTokens(address beneficiary, uint amount
+    ) onlyCreator returns (bool success) {
+        if (sealed()) throw;              
+        balances[beneficiary] += amount;  // Create Tokens for the beneficiary
+        totalSupply += amount;            // Update total supply
+        Transfer(0, beneficiary, amount); // Create an Event for the creation
+        return true; 
     }
 
+// seal can only be called by the Creator and it makes it impossible to create
+// more tokens by changing the creator to 0
 
     function seal() onlyCreator returns (bool success)  {
         creator = 0;
         return true;
     }
+
+// sealed checks to see if the the token contract can create tokens
 
     function sealed() constant returns (bool) {
         return creator == 0;
