2c4b36d913a9852563eb3c433514d94a8a5f3dfb
Griff Green <saveoursonics@gmail.com>
2016-11-01 02:54:44 +0800

    Conformed comments to NatSpec Format
    :-) Over Commented!

--- TokenCreator.sol
+++ TokenCreator.sol
@@ -1,23 +1,30 @@
 pragma solidity ^0.4.0;
 
-// This contract creates Charity Tokens using CharityToken.sol
-
 import "CharityToken.sol";
+
+/// @title CharityToken Contract 
+/// @author Jordi Baylina
+/// @dev This is designed to control the ChairtyToken contract.
 
 contract TokenCreator {
 
     uint public startFundingTime;       // In UNIX Time Format
     uint public endFundingTime;         // In UNIX Time Format
+    uint public maximumFunding;         // In wei
+    uint public totalCollected;         // In wei
+    CharityToken public tokenContract;  // The new token for this Campaign
+    address public vaultContract;       // The address to hold the funds donated
 
-    uint public maximumFunding;         // In wei??
-
-    uint public totalCollected;         // In wei??
-
-    CharityToken public tokenContract;  // The new Campaign token
-    address public vaultContract;       // The Address to hold the funds donated
-
-// 'TokenCreator()' created the new Campaign Token with the begining and ending
-// Unix times the max funding allowed and the address to hold the donations
+/// @notice 'TokenCreator()' initiates the Campaign by setting its funding 
+/// parameters and creating the deploying the token contract
+/// @dev There are several checks to make sure the parameters are acceptable 
+/// @param _startFundingTime The UNIX time that the Campaign will be able to 
+/// start receiving funds
+/// @param _endFundingTime The UNIX time that the Campaign will stop being able 
+/// to receive funds
+/// @param _maximumFunding In wei, the Maximum amount that the Campaign can
+/// receive (currently the max is set at 10,000 ETH for the beta)
+/// @param _vaultContract The address that will store the donated funds
 
     function TokenCreator(
         uint _startFundingTime,
@@ -27,7 +34,7 @@ contract TokenCreator {
     ) {
         if ((_endFundingTime < now) ||                //Cannot start in the past
             (_endFundingTime <= _startFundingTime) ||
-            (_maximumFunding > 10000 ether) ||        //Can be increased later
+            (_maximumFunding > 10000 ether) ||        //The Beta is limited
             (_vaultContract == 0))                    //To prevent burning ETH
             {
             throw;
@@ -35,21 +42,35 @@ contract TokenCreator {
         startFundingTime = _startFundingTime;
         endFundingTime = _endFundingTime;
         maximumFunding = _maximumFunding;
-        tokenContract = new CharityToken ();
+        tokenContract = new CharityToken (); // Deploys the Token Contract
         vaultContract = _vaultContract;
     }
-// Fallback function: when a normal send of ether is made to this contract
-// `doPayment()' is called witht he msg.sender as the owner
+    
+/// @dev The fallback function is called when ether is sent to the contract, it
+/// simply calls `doPayment()` with the address that sent the ether as the
+/// `_owner`. Payable is a required solidity modifier for functions to receive 
+/// ether, without this modifier they will throw
+
     function ()  payable {
         doPayment(msg.sender);
     }
 
+/// @notice `proxyPayment()` allows the caller to send ether to the Campaign and
+/// have the CharityTokens created in an address of their choosing
+/// @param _owner The address that will hold the newly created CharityTokens
+
     function proxyPayment(address _owner) payable {
         doPayment(_owner);
     }
+    
+/// @dev `doPayment()` is an internal function that sends the ether that this 
+/// contract receives to the `vaultContract` and creates charityTokens in the 
+/// address of the `_owner` assuming the Campaign is still accepting funds
+/// @param _owner The address that will hold the newly created CharityTokens
 
     function doPayment(address _owner) internal {
-// First we check that the Campaign can receive the value being sent to it
+
+// First we check that the Campaign is allowed to receive this donation
         if ((now<startFundingTime) ||
             (now>endFundingTime) ||
             (msg.value == 0) ||
@@ -57,21 +78,28 @@ contract TokenCreator {
         {
             throw;
         }
+
 //Track how much the Campaign has collected
         totalCollected += msg.value;
-//Sends the ETH to the Vault
+
+//Send the ether to the vaultContract
         if (!vaultContract.send(msg.value)) {
             throw;
         }
-// Creates an equal amount of Campaign Tokens as ether sent. The Campaign Tokens
-// are created in the address the doner specified (or sent from).
+
+// Creates an equal amount of CharityTokens as ether sent. The new CharityTokens
+// are created in the `_owner` address
         if (!tokenContract.createTokens(_owner, msg.value)) {
             throw;
         }
 
         return;
     }
-// When `seal()` is called no other funds can be sent to this Campaign
+
+/// @notice `seal()` ends the Campaign by calling `seal()` in the CharityToken 
+/// contract
+/// @dev `seal()` can only be called after the end of the funding period.
+
     function seal() {
         if (now < endFundingTime) throw;
         tokenContract.seal();
