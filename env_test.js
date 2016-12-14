751879188a5d2c77bb15414058ae21612c50e967
Jordi Baylina <jordi@baylina.cat>
2016-12-14 12:15:47 +0100

    More deploy in test

--- env_test.js
+++ env_test.js
@@ -3,6 +3,9 @@ var Web3 = require('web3');
 // create an instance of web3 using the HTTP provider.
 // NOTE in mist web3 is already available, so check first if its available before instantiating
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+var BigNumber = require('bignumber.js');
+
 var milestoneTrackerHelper = require('milestonetracker');
 
 var eth = web3.eth;
