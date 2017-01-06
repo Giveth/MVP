ee57642617d68843d68f4f5b7826cd636e369506
Griff Green <saveoursonics@gmail.com>
2017-01-06 21:43:59 +0700

    Removed Bug bounty proposals added website, Airbnb

--- GivethJanMilestones.js
+++ GivethJanMilestones.js
@@ -4,8 +4,9 @@ var Griff = "0x839395e20bbb182fa440d08f850e6c7a8f6f0780";   // Griff
 var Gian  = "0x82aEB1D8939f514318449fa8Ec704A94DC16E01D";   // Gian
 var Jordi = "0x1dba1131000664b884a1ba238464159892252d3a";   // Jordi
 var Grace = "0x64A1C14aCF4D3d1F8F0316f6C924Ec8f35a27BCF";
+// var Vojtech = ""
 var MultisigAddy = "0x32bacc8B241FB172fEE18bDa32527126c6f3c5f7";
-var ETHprice = 7.70;
+var ETHprice = 10;
 
 
 
@@ -16,10 +17,11 @@ var vault = web3.eth.contract(vaultAbi).at(vaultAddr);
 
 
 var milestonesBytes = milestoneTrackerHelper.milestones2bytes( {
-        description:"Basic UI for Milestone Tracker: Developing the basic UI architecture for the Milestone Tracker Contract enabling anyone using MetaMask, Mist, or Parity to generate new milestones and effectively implement all features in the Milestone Tracker Contract, this basic UI will be focused on functionality and enabling integration and easy customization for this UI to be added to other websites of various designs: 5000 USD in ETH at the market rate to Jordi and Friends",
+        description:"Basic UI for Milestone Tracker: Developing the basic UI architecture for the Milestone Tracker Contract enabling anyone using MetaMask, Status.im, Mist, or Parity to generate new milestones and effectively implement all features in the Milestone Tracker Contract, this basic UI will be focused on functionality, enabling integration and easy customization for this UI to be added to other websites of various designs: 5000 USD in ETH at the market rate to Jordi and Vojtech",
         url:"http://www.giveth.io",
         minDoneDate:Math.floor(new Date('2017-01-01').getTime() /1000),
         maxDoneDate:Math.floor(new Date('2017-04-01').getTime() /1000),
+        milestoneLeadLink:Jordi,
         reviewer:Jordi,
         reviewTime:86400*7,
         payDestination:vaultAddr,
@@ -30,10 +32,11 @@ var milestonesBytes = milestoneTrackerHelper.milestones2bytes( {
             0
         )
     }, {
-        description:"Basic Governance Smart Contract: Designing and developing a basic DAO-like Smart Contract to allow the token holders to use their tokens to make group decisions (e.g. to determine if the milestone should be marked complete or not), and deploying it on the Ethereum blockchain to be used with the Giveth Campaign after thorough testing: 2500 USD in ETH at the market rate to Jordi and Friends",
+        description:"Basic Governance Smart Contract: Designing and developing a basic DAO-like Smart Contract to allow the token holders to use their tokens to make group decisions (e.g. to determine if the completion of a milestone is valid(acting as a reviewer) and to accept  ), and deploying it on the Ethereum blockchain to be used with the Giveth Campaign after thorough testing: 2500 USD in ETH at the market rate to Jordi and Friends",
         url:"http://www.giveth.io",
         minDoneDate:Math.floor(new Date('2017-01-01').getTime() /1000),
         maxDoneDate:Math.floor(new Date('2017-04-01').getTime() /1000),
+        milestoneLeadLink:Jordi,
         reviewer:Jordi,
         reviewTime:86400*7,
         payDestination:vaultAddr,
@@ -44,10 +47,11 @@ var milestonesBytes = milestoneTrackerHelper.milestones2bytes( {
             0
         )
     }, {
-        description:"Delegated Pledging Smart Contract Research: We are still in the research phase, this may not be possible due to gas constraints and game theoretical attacks. Designing a basic liquid democracy-like Smart Contract to allow Donors to send their tokens to delegates so that  decisions about which project to invest in can be done by the Delegate, not the donor is much more complicated than expected. This milestone will be completed when a design is understood and work can begin on developing a smart contract that can be deployed on the Ethereum blockchain: 2500 USD in ETH at the market rate to Jordi and Friends",
+        description:"Delegated Pledging Smart Contract Research: We are still in the research phase, this may not be possible due to gas constraints and game theoretical attacks. Designing a basic liquid democracy-like Smart Contract (to allow Donors to send their tokens to delegates so that decisions about which project to invest in can be done by the Delegate, not the donor) is much more complicated than expected. This milestone will be completed when a design is understood and work can begin on developing a smart contract that can be deployed on the Ethereum blockchain: 2500 USD in ETH at the market rate to Jordi and Friends",
         url:"http://www.giveth.io",
         minDoneDate:Math.floor(new Date('2017-01-01').getTime() /1000),
         maxDoneDate:Math.floor(new Date('2017-07-01').getTime() /1000),
+        milestoneLeadLink:Jordi,
         reviewer:Jordi,
         reviewTime:86400*7,
         payDestination:vaultAddr,
@@ -58,52 +62,11 @@ var milestonesBytes = milestoneTrackerHelper.milestones2bytes( {
             0
         )
     }, {
-        description:"The Vault's Hack our Honeypot Bug Bounty Program: To incentivize hackers to attack our vault, we will crowd source the funds that will be stored in the vault and pay interest to the participants willing to put their ETH at risk. Anyone that wants to participate can deposit their ETH into our vault and once 5000 ETH has been stored in the vault for 30 days, the 5000 ETH along with an extra 200 ETH worth of interest (this is an APR of about 48%!) will be paid out proportionally to the participants. Also worth noting, the Giveth Core Devs will also try to hack the vault, but if we succeed, we will return the funds to the original owners, but the 200 ETH will not be paid out to anyone.",
-        url:"http://www.giveth.io",
-        minDoneDate:Math.floor(new Date('2017-01-01').getTime() /1000),
-        maxDoneDate:Math.floor(new Date('2017-05-01').getTime() /1000),
-        reviewer:Jordi,
-        reviewTime:86400*7,
-        payDestination:vaultAddr,
-        payData:vault.authorizePayment.getData(
-            "Hack our Honeypot Bug Bounty",
-            MultisigAddy,
-            web3.toWei(200),
-            0
-        )
-    }, {
-        description: "Giveth's Bug Bounty Program: During the Vault's Hack our Honeypot Bug Bounty program, if other smaller bugs are found and issues are posted on our github the lead to coding changes Griff and Jordi will happily reward the issue creators anywhere between 1 and 30 ETH depending on the severity of the bug (1 ETH for cosmetic fixes, 2-10 ETH for gas optimization and 10-30 ETH for critical bug discovery). All issues created before the end of the vault's 30 day holding period ends will be paid out if they lead to code changes, after that issues that lead to code changes will not be paid out, they will be made out of the goodness of the github user's heart... as is customary in open source projects :-)."
-        url:"http://www.giveth.io",
-        minDoneDate:Math.floor(new Date('2017-01-01').getTime() /1000),
-        maxDoneDate:Math.floor(new Date('2017-05-01').getTime() /1000),
-        reviewer:Jordi,
-        reviewTime:86400*7,
-        payDestination:vaultAddr,
-        payData:vault.authorizePayment.getData(
-            "Giveth Bug Bounty",
-            MultisigAddy,
-            web3.toWei(150),
-            0
-        )
-    }, {
-        description: "Building and Distributing Giveth's Bug Bounty Program: Designing the code that allows the Vault bug bounty to happen (xxx to Jordi) and promoting and explaining the bug bounty on social media (Griff will do for free) distributing ETH to the people that find issues and integrating the code changes (xxx Griff and Jordi), DAO.Link managing the legal protection for the program (xxx to DAO.link (Grace? :-D)) "
-        url:"http://www.giveth.io",
-        minDoneDate:Math.floor(new Date('2017-01-01').getTime() /1000),
-        maxDoneDate:Math.floor(new Date('2017-05-01').getTime() /1000),
-        reviewer:Jordi,
-        reviewTime:86400*7,
-        payDestination:vaultAddr,
-        payData:vault.authorizePayment.getData(
-            "The creation of Giveth's Bug Bounty Programs",
-            MultisigAddy,
-            web3.toWei(xxx),
-            0
-        )
-    }, {
         description: "Reviewing contracts and potentially adding invariance to the vault: Reviewing the delegated pledging contract, the basic DAO contract and any other crazy ideas Jordi comes up with :-) looking for bugs and making the final approval before deploying live on the blockchain: $300 USD in ETH at the market rate to Barry White",
         url:"http://www.giveth.io",
         minDoneDate:Math.floor(new Date('2017-01-01').getTime() /1000),
         maxDoneDate:Math.floor(new Date('2017-05-01').getTime() /1000),
+        milestoneLeadLink:Jordi,
         reviewer:Jordi,
         reviewTime:86400*7,
         payDestination:vaultAddr,
@@ -114,11 +77,42 @@ var milestonesBytes = milestoneTrackerHelper.milestones2bytes( {
             0
         )
     }, {
-        description:"Promote Giveth and build the webstie and do all the little things that make this project work (TODO FIGURE THIS OUT) xxx in ETH at the market rate to Grace Torreilas",
+        description:"The Airbnb in London for Grace, Griff, Jordi, and Vojtech. We are launching Giveth officially to the public at the Blockchain-Expo and we are staying close to Colony's offices in Shoreditch so after the Expo we can look to collaborate with Colony as much as possible as both projects evolve. The Airbnb is to be reimbursed in full: $1164 USD in ETH at the market rate to Griff Green, the URL shows the receipt posted on IPFS",
+        url:"https://ipfs.pics/QmX6m4K59WHJRcPjVSUE2p56hCqADpQoevcEvbDxZwStsQ",
+        minDoneDate:Math.floor(new Date('2017-01-01').getTime() /1000),
+        maxDoneDate:Math.floor(new Date('2017-05-01').getTime() /1000),
+        milestoneLeadLink:Griff,
+        reviewer:Griff,
+        reviewTime:86400*7,
+        payDestination:vaultAddr,
+        payData:vault.authorizePayment.getData(
+            "London Airbnb",
+            Griff,
+            web3.toWei(1164/ETHprice),
+            0
+        )
+    }, {
+        description:"Build the Giveth Website make it beautiful and functional... I don't know how to break that up (TODO Figure this out) :-D xxx in ETH at the market rate to Grace Torrellas",
         url:"http://www.giveth.io",
         minDoneDate:Math.floor(new Date('2017-01-01').getTime() /1000),
         maxDoneDate:Math.floor(new Date('2017-05-01').getTime() /1000),
-        reviewer:Jordi,
+        milestoneLeadLink:Grace,
+        reviewer:Griff,
+        reviewTime:86400*7,
+        payDestination:vaultAddr,
+        payData:vault.authorizePayment.getData(
+            "Making the website",
+            Grace,
+            web3.toWei(1500/ETHprice),
+            0
+        )
+    }, {
+        description:"Promote Giveth and manage the Giveth Brand, this involves building the webstie and do all the little things that make this project work (TODO FIGURE THIS OUT) xxx in ETH at the market rate to Grace Torrellas",
+        url:"http://www.giveth.io",
+        minDoneDate:Math.floor(new Date('2017-01-01').getTime() /1000),
+        maxDoneDate:Math.floor(new Date('2017-05-01').getTime() /1000),
+        milestoneLeadLink:Grace,
+        reviewer:Griff,
         reviewTime:86400*7,
         payDestination:vaultAddr,
         payData:vault.authorizePayment.getData(
@@ -132,7 +126,8 @@ var milestonesBytes = milestoneTrackerHelper.milestones2bytes( {
         url:"http://www.giveth.io",
         minDoneDate:Math.floor(new Date('2017-01-01').getTime() /1000),
         maxDoneDate:Math.floor(new Date('2017-05-01').getTime() /1000),
-        reviewer:Jordi,
+        milestoneLeadLink:Jordi,
+        reviewer:Griff,
         reviewTime:86400*7,
         payDestination:vaultAddr,
         payData:vault.authorizePayment.getData(
